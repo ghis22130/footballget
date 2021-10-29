16d603811bd75f1a67a76bf265ce5fbfb11a581e
JiSoo Kim <ghis22130@naver.com>
2021-10-29 09:28:23 +0900

    feat: dynamic Timzone (#10)

--- FootballGet/FootballGetWidget/Network/NetworkRequest/NextMatchUpRequest.swift
+++ FootballGet/FootballGetWidget/Network/NetworkRequest/NextMatchUpRequest.swift
@@ -18,12 +18,14 @@ final class NextMatchUpRequest: NetworkRequest {
     var body: Data?
     
     init(_ teamId: String) {
+        let timeZone = TimeZone.current.identifier
+        
         self.method = .get
         self.path = "fixtures"
         self.queryItems = ["season": "2021",
                            "team": teamId,
                            "next": "1",
-                           "timezone": "Asia/seoul"]
+                           "timezone": timeZone]
     }
     
 }
